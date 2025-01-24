962983284b324a4af00ba7dd3f84f7eedecaa7b7
Mamun <omolajababatunde149@gmail.com>
2025-01-24 22:35:54 +0100

    makes some changes

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ Console.ResetColor();
 Console.Clear();
 Console.WriteLine("Select An option from the list below");
 Console.WriteLine("1. Addition\n2.Subtraction\n3.Division\n4.Multiplication\n5.power off");
-string userChoice = Console.ReadLine();
+bool userInput = int.TryParse(Console.ReadLine(), out int value);
+if(userInput == false)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Choose Number Oga");
+    Console.ResetColor();
+}
+string userChoice = Console.ReadLine()!;
 if (userChoice == "1")
 {
 
     Console.WriteLine("Enter First Number: ");
-    var first = int.Parse(Console.ReadLine());
+    var first = int.Parse(Console.ReadLine()!);
     Console.WriteLine("Enter Second Number: ");
     var second = Convert.ToInt32(Console.ReadLine());
 
@@ -18,7 +25,7 @@ if (userChoice == "1")
 else if (userChoice == "2")
 {
     Console.WriteLine("Enter First Number: ");
-    var first = int.Parse(Console.ReadLine());
+    var first = int.Parse(Console.ReadLine()!);
     Console.WriteLine("Enter Second Number: ");
     var second = Convert.ToInt32(Console.ReadLine());
 
@@ -28,7 +35,7 @@ else if (userChoice == "2")
 else if (userChoice == "3")
 {
     Console.WriteLine("Enter First Number: ");
-    var first = int.Parse(Console.ReadLine());
+    var first = int.Parse(Console.ReadLine()!);
     Console.WriteLine("Enter Second Number: ");
     var second = Convert.ToInt32(Console.ReadLine());
 
@@ -37,7 +44,7 @@ else if (userChoice == "3")
 else if (userChoice == "4")
 {
     Console.WriteLine("Enter First Number: ");
-    var first = int.Parse(Console.ReadLine());
+    var first = int.Parse(Console.ReadLine()!);
     Console.WriteLine("Enter Second Number: ");
     var second = Convert.ToInt32(Console.ReadLine());
 
